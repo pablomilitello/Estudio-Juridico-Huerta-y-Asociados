3b6db2d784382a270162a064224d9401e359fd05
“Pablo <pmilitelloaveni@gmail.com>
2023-03-29 15:17:10 -0300

    agrego libreria Toastify

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
 const contactForm = document.getElementById('contactForm');
+const nameForm = document.getElementById('nameForm').value;
+const lastnameForm = document.getElementById('lastnameForm').value;
+const emailForm = document.getElementById('emailForm').value;
+const phoneForm = document.getElementById('phoneForm').value;
+const commentForm = document.getElementById('commentForm').value;
+const dataInput = { nameForm, lastnameForm, emailForm, phoneForm, commentForm };
+const dataJson = JSON.stringify(dataInput);
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const nameForm = document.getElementById('nameForm').value;
-  const lastnameForm = document.getElementById('lastnameForm').value;
-  const emailForm = document.getElementById('emailForm').value;
-  const phoneForm = document.getElementById('phoneForm').value;
-  const commentForm = document.getElementById('commentForm').value;
-  const dataInput = { nameForm, lastnameForm, emailForm, phoneForm, commentForm };
-  const dataJson = JSON.stringify(dataInput);
-
   fetch('http://localhost:8080/inputData', {
-    method: 'Post',
+    method: 'POST',
     body: dataJson,
   });
+
+  Toastify({
+    text: 'Su consulta ha sido enviada exitosamente!',
+    duration: 2500,
+    newWindow: true,
+    close: true,
+    gravity: 'top',
+    position: 'right',
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: 'linear-gradient(to right, #00b09b, #96c93d)',
+    },
+    callback: function () {
+      contactForm.reset();
+    },
+  }).showToast();
 });
--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -2,12 +2,17 @@ import { Router } from 'express';
 
 const router = Router();
 
+const arrayInfo = [];
+
 router.get('/', (req, res) => {
   res.send('HOLA DESDE EL BACKEND USANDO ROUTES');
 });
 
 router.post('/inputData', (req, res) => {
-  console.log(req.body);
+  let info = req.body;
+  arrayInfo.push(info);
+  console.log(arrayInfo);
+  res.send('Info guardada con éxito!');
 });
 
 export default router;
